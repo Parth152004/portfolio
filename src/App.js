0ee9ba55b7b68a3afb47148689f29a384e92d68e
Parth152004 <patelparth1167@gmail.com>
2024-06-03 13:21:47 +0530

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+import Home from './Component/Home';
+import Skill from './Component/Skill';
+import Contect from './Component/Contect';
+import About from './Component/About';
+import Navbar from './Sidecomponent/Navbar';
+import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/Skill' element={<Skill/>}></Route>
+        <Route path='/Contect' element= {<Contect/>}></Route>
+        <Route path='/About' element= {<About/>} ></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
